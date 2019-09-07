16859e1d86700fd338f27b202c9b9dfa39f897cd
king <3603317@qq.com>
2019-09-07 17:35:40 +0800

    当textarea设置了value的时候,必须设置onChange或是readonly

--- src/pages/components/Code.jsx
+++ src/pages/components/Code.jsx
@@ -2,14 +2,14 @@
  * @Author: houxingzhang
  * @Date: 2019-09-05 17:16:54
  * @Last Modified by: houxingzhang
- * @Last Modified time: 2019-09-05 17:17:15
+ * @Last Modified time: 2019-09-07 17:34:46
  */
 import React from 'react'
 
 export default props => {
   return (
     <div>
-      <textarea className='page_designer_code' value={props.code}>
+      <textarea className='page_designer_code' value={props.code} readOnly>
         源代码预览区
       </textarea>
     </div>
